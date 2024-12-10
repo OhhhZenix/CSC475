32ead4d2a9351af91516e30602e48018ea33ece3
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-12-10 11:57:11 -0700

    Update MainActivity.kt

--- Module_1/app/src/main/java/com/github/ohhhzenix/csc475/module1/MainActivity.kt
+++ Module_1/app/src/main/java/com/github/ohhhzenix/csc475/module1/MainActivity.kt
@@ -9,12 +9,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
