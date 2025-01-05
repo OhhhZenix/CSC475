eb3655c069ea167d08066effd12d9ce70f74dcf7
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 15:28:13 -0700

    Update TaskDao.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/database/TaskDao.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/database/TaskDao.kt
@@ -4,16 +4,23 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
     @Query("SELECT * FROM task")
-    fun getAll(): List<Task>
+    fun getAll(): Flow<List<Task>>
+
+    @Query("SELECT * FROM task WHERE completed = 1")
+    fun getAllCompleted(): Flow<List<Task>>
+
+    @Query("SELECT * FROM task WHERE completed = 0")
+    fun getAllUncompleted(): Flow<List<Task>>
 
     @Upsert
-    suspend fun add(task: Task)
+    suspend fun upsertTask(task: Task)
 
     @Delete
-    suspend fun remove(task: Task)
+    suspend fun deleteTask(task: Task)
 }
