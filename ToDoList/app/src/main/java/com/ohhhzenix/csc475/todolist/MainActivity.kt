3d89fc09d15572ef43316cf6452ba1130ff2b877
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 21:35:01 -0700

    Update MainActivity.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainActivity.kt
@@ -3,26 +3,51 @@ package com.ohhhzenix.csc475.todolist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.ohhhzenix.csc475.todolist.ui.theme.ToDoListTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         val taskViewModel = ViewModelProvider(this)[TaskViewModel::class.java]
+
         setContent {
             ToDoListTheme {
-                Surface(
-                    modifier = Modifier.fillMaxHeight()
-                ) {
-                    MainScreen(taskViewModel)
-                }
+                AppScreens(viewModel = taskViewModel)
             }
         }
     }
 }
+
+@Composable
+fun AppScreens(viewModel: TaskViewModel) {
+    val navController = rememberNavController()
+    val filter by remember { mutableStateOf(FilterType.SHOW_UNCOMPLETED) }
+
+    NavHost(
+        navController = navController,
+        startDestination = AppScreen.Main.name
+    ) {
+        composable(AppScreen.Main.name) {
+            HomeScreen(viewModel, navController)
+        }
+        composable(AppScreen.Filter.name) {
+            FilterScreen(navController, filter)
+        }
+        composable(AppScreen.NewTask.name) {
+            NewTaskScreen(viewModel)
+        }
+    }
+}
+
+
