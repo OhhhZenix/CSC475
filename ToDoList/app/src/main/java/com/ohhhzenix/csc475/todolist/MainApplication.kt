7079203bc75f88acad2755bd43332711cede8f3e
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 18:35:00 -0700

    Update MainApplication.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainApplication.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainApplication.kt
@@ -1,4 +1,21 @@
 package com.ohhhzenix.csc475.todolist
 
-class MainApplication {
+import android.app.Application
+import androidx.room.Room
+import com.ohhhzenix.csc475.todolist.database.TaskDatabase
+
+class MainApplication : Application() {
+
+    companion object {
+        lateinit var taskDatabase: TaskDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Room.databaseBuilder(
+            applicationContext,
+            TaskDatabase::class.java,
+            TaskDatabase.NAME
+        )
+    }
 }
