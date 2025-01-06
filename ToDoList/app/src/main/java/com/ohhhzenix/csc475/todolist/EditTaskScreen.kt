175612dfd080e5174d1df2b0ab7ca2b2f22a9454
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 18:07:11 -0700

    Update EditTaskScreen.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/EditTaskScreen.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/EditTaskScreen.kt
@@ -1,4 +1,92 @@
 package com.ohhhzenix.csc475.todolist
 
-class EditTaskScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ohhhzenix.csc475.todolist.database.Task
+
+@Preview
+@Composable
+fun EditTaskScreen(task: Task = Task(0, "", "")) {
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Button(
+            onClick = {}
+        ) {
+            Text("Back")
+        }
+        Spacer(Modifier.padding(30.dp))
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "Edit Task",
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(modifier = Modifier.padding(2.dp))
+                Text(
+                    "(Currently selected id: ${task.id})",
+                    color = Color.LightGray,
+                    fontSize = 12.sp,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+        Spacer(Modifier.padding(8.dp))
+        OutlinedTextField(
+            value = task.title,
+            onValueChange = {},
+            label = {
+                Text("Title")
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        OutlinedTextField(
+            value = task.description,
+            onValueChange = {},
+            label = {
+                Text("Description")
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(Modifier.padding(4.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(if (task.completed) "Unmark as completed" else "Mark as completed")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Delete task")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Save task")
+        }
+    }
 }
