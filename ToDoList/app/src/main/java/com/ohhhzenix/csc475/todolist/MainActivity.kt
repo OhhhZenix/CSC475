93cef2f5a3e5b02226d09588e08f8c9719dce158
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 19:00:24 -0700

    Update MainActivity.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainActivity.kt
@@ -3,23 +3,24 @@ package com.ohhhzenix.csc475.todolist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
 import com.ohhhzenix.csc475.todolist.ui.theme.ToDoListTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
+        val taskViewModel = ViewModelProvider(this)[TaskViewModel::class.java]
         setContent {
             ToDoListTheme {
                 Surface(
                     modifier = Modifier.fillMaxHeight()
                 ) {
-                    MainScreen()
+                    MainScreen(taskViewModel)
                 }
             }
         }
