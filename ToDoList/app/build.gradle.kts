f1d0667364783d07122d2024461f48012b4f3fa8
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 18:55:15 -0700

    Update build.gradle.kts

--- ToDoList/app/build.gradle.kts
+++ ToDoList/app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
+    implementation(libs.androidx.runtime.livedata)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
