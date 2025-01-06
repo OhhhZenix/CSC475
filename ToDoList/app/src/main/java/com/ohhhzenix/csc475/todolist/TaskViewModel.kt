6bea7355df747f89acc4112f5056a5f34a72ea8b
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 18:40:15 -0700

    Update TaskViewModel.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/TaskViewModel.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/TaskViewModel.kt
@@ -1,4 +1,31 @@
 package com.ohhhzenix.csc475.todolist
 
-class TaskViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.ohhhzenix.csc475.todolist.database.Task
+
+class TaskViewModel : ViewModel() {
+
+    val taskDao = MainApplication.taskDatabase.getTaskDao();
+    val tasks: LiveData<List<Task>> = taskDao.getAll()
+
+    fun getAll(): List<Task> {
+        return emptyList()
+    }
+
+    fun getAllCompleted() {
+
+    }
+
+    fun getAllUncompleted() {
+
+    }
+
+    fun addTask(title: String, description: String) {
+        taskDao.upsertTask(Task(title = title, description = description))
+    }
+
+    fun deleteTask(id: Int) {
+        taskDao.deleteTask(id)
+    }
 }
