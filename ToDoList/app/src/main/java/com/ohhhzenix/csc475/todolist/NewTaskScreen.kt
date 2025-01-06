8bc6d24eedd746e906af36256c9131206bfc4648
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-05 18:08:14 -0700

    Update NewTaskScreen.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/NewTaskScreen.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/NewTaskScreen.kt
@@ -1,4 +1,64 @@
 package com.ohhhzenix.csc475.todolist
 
-class NewTaskScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun NewTaskScreen() {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Button(onClick = {}) {
+            Text("Back")
+        }
+        Spacer(Modifier.padding(30.dp))
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                "New Task",
+                color = Color.White,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+            )
+        }
+        Spacer(Modifier.padding(8.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            label = {
+                Text("Title")
+            }
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            label = {
+                Text("Description")
+            },
+        )
+        Spacer(Modifier.padding(8.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {}
+        ) {
+            Text("Add")
+        }
+    }
 }
