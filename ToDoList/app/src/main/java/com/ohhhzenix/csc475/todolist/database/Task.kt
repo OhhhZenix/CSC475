e68c41765a581ed1775dde18c25ae6b0238a3698
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-06 22:22:01 -0700

    Update Task.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/database/Task.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/database/Task.kt
@@ -4,9 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "tasks")
 data class Task(
-    @PrimaryKey(autoGenerate = true) val id: Int = System.currentTimeMillis().toInt(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "description") val description: String,
     @ColumnInfo(name = "completed") val completed: Boolean = false
