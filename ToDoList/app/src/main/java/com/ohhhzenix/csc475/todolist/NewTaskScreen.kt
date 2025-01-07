f463581c44ec4ccd4b552bfa40833aaee8e9a2b4
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-06 22:23:42 -0700

    Update NewTaskScreen.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/NewTaskScreen.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/NewTaskScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -102,7 +101,12 @@ fun NewTaskScreen(
                             .show()
                     } else {
                         coroutineScope.launch {
-                            taskDao.upsertTask(Task(title = title.value, description = description.value))
+                            taskDao.upsertTask(
+                                Task(
+                                    title = title.value,
+                                    description = description.value
+                                )
+                            )
                         }
                         navController.navigate(AppScreen.Main.name)
                     }
