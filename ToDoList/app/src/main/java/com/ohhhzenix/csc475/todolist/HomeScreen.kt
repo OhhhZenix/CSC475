06f7eb61e360d42ae7398b5b1a84733559042a57
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-06 18:33:28 -0700

    Update HomeScreen.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/HomeScreen.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/HomeScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
@@ -26,44 +29,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ohhhzenix.csc475.todolist.database.Task
 
-@Composable
-fun ToDoItem(task: Task) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .clip(shape = RoundedCornerShape(16.dp))
-            .background(color = MaterialTheme.colorScheme.secondary)
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(task.title, color = Color.White, modifier = Modifier.weight(1f))
-        IconButton(onClick = {}) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_check_box_24),
-                contentDescription = "Complete",
-                tint = Color.White
-            )
-        }
-        IconButton(onClick = {}) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_delete_24),
-                contentDescription = "Delete",
-                tint = Color.White
-            )
-        }
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(viewModel: TaskViewModel, navController: NavController) {
-    val tasks by viewModel.getAll().observeAsState()
-
+fun HomeScreen(
+    navController: NavController,
+    tasks: MutableList<Task>,
+    selectedTask: MutableState<Task?>,
+    filter: MutableState<FilterType>
+) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -104,8 +83,58 @@ fun HomeScreen(viewModel: TaskViewModel, navController: NavController) {
                 }
             }
             LazyColumn(content = {
-                items(tasks.orEmpty()) { item: Task ->
-                    ToDoItem(task = item)
+                val filteredTask = when (filter.value) {
+                    FilterType.SHOW_ALL -> tasks.sortedBy { it.completed }
+                    FilterType.SHOW_COMPLETED -> tasks.filter { it.completed }
+                    FilterType.SHOW_UNCOMPLETED -> tasks.filter { !it.completed }
+                }
+
+                items(filteredTask) { task: Task ->
+                    Button(
+                        onClick = {
+                            selectedTask.value = task
+                            navController.navigate(AppScreen.EditTask.name)
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .height(80.dp),
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonColors(
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            contentColor = Color.White,
+                            disabledContainerColor = MaterialTheme.colorScheme.secondary,
+                            disabledContentColor = MaterialTheme.colorScheme.secondary
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier.background(color = MaterialTheme.colorScheme.secondary),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painter = painterResource(
+                                    id = if (task.completed)
+                                        R.drawable.baseline_check_box_24
+                                    else
+                                        R.drawable.baseline_check_box_outline_blank_24
+                                ),
+                                contentDescription = "Delete",
+                                tint = Color.White
+                            )
+                            Spacer(Modifier.padding(8.dp))
+                            Text(
+                                task.title,
+                                color = Color.White,
+                                modifier = Modifier.weight(1f),
+                                style = TextStyle(
+                                    textDecoration = if (task.completed)
+                                        TextDecoration.LineThrough
+                                    else
+                                        TextDecoration.None
+                                )
+                            )
+                        }
+                    }
                 }
             })
         }
