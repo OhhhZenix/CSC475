b8532607131aef6f694fd7009e973a2559f83efe
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-06 22:22:14 -0700

    Update MainActivity.kt

--- ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/ohhhzenix/csc475/todolist/MainActivity.kt
@@ -5,53 +5,52 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.ohhhzenix.csc475.todolist.database.Task
+import com.ohhhzenix.csc475.todolist.database.TaskDao
 import com.ohhhzenix.csc475.todolist.ui.theme.ToDoListTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val taskDao = MainApplication.appDatabase.getTaskDao();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val taskViewModel = ViewModelProvider(this)[TaskViewModel::class.java]
 
         setContent {
             ToDoListTheme {
-                MyApp(viewModel = taskViewModel)
+                MyApp(taskDao)
             }
         }
     }
 }
 
 @Composable
-fun MyApp(viewModel: TaskViewModel) {
+fun MyApp(taskDao: TaskDao) {
     val navController = rememberNavController()
-    val tasks = remember { mutableListOf<Task>() }
     val filter = remember { mutableStateOf(FilterType.SHOW_UNCOMPLETED) }
-    val selectedTask = remember { mutableStateOf<Task?>(null) }
+    val selectedTask = remember { mutableIntStateOf(0) }
 
     NavHost(
         navController = navController,
         startDestination = AppScreen.Main.name
     ) {
         composable(AppScreen.Main.name) {
-            HomeScreen(navController, tasks, selectedTask, filter)
+            HomeScreen(navController, filter, selectedTask, taskDao)
         }
         composable(AppScreen.Filter.name) {
             FilterScreen(navController, filter)
         }
         composable(AppScreen.NewTask.name) {
-            NewTaskScreen(navController, tasks)
+            NewTaskScreen(navController, taskDao)
         }
         composable(AppScreen.EditTask.name) {
-            EditTaskScreen(navController, selectedTask, tasks)
+            EditTaskScreen(navController, selectedTask, taskDao)
         }
     }
 }
