2a9b9db592d19177b41b731cd0e26af6d0765518
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-16 12:05:52 -0700

    Update InternetScreen.kt

--- PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/InternetScreen.kt
+++ PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/InternetScreen.kt
@@ -1,6 +1,50 @@
 package com.ohhhzenix.csc475.photogallery
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InternetScreen() {}
+fun InternetScreen(
+    navHomeScreen: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Internet View")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Button(
+                onClick = navHomeScreen,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text("Back")
+            }
+            Spacer(Modifier.padding(16.dp))
+        }
+    }
+}
