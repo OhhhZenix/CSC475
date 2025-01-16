ddeb4568a1faaa59a5d657e753575631b4826132
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-16 12:29:55 -0700

    Update InternetScreen.kt

--- PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/InternetScreen.kt
+++ PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/InternetScreen.kt
@@ -1,25 +1,39 @@
 package com.ohhhzenix.csc475.photogallery
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InternetScreen(
     navHomeScreen: () -> Unit
 ) {
+    val context = LocalContext.current
+    var searchQuery by remember { mutableStateOf("") }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -45,6 +59,44 @@ fun InternetScreen(
                 Text("Back")
             }
             Spacer(Modifier.padding(16.dp))
+            Text(
+                "Get Started",
+                Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                style = TextStyle(
+                    fontSize = 32.sp
+                )
+            )
+            OutlinedTextField(
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+                label = {
+                    Text("Search")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp)
+            )
+            Button(
+                onClick = {
+                    if (searchQuery.isEmpty()) {
+                        Toast.makeText(context, "Search is empty. Try again.", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text("Search")
+            }
+            Text(
+                "No results.",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
