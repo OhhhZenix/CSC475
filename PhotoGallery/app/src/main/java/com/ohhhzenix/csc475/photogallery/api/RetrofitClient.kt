261d1c02aff6daf12c6e99338973359cc3d92875
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-16 15:47:57 -0700

    Update RetrofitClient.kt

--- PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/api/RetrofitClient.kt
+++ PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    val dogApi by lazy {
+    val dogApi: DogApi by lazy {
         Retrofit.Builder()
             .baseUrl("https://dog.ceo/api/")
             .addConverterFactory(GsonConverterFactory.create())
