a2ec7b3ca86a3192ebbe084351afae1a524f7303
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-16 15:43:26 -0700

    Update InternetScreen.kt

--- PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/InternetScreen.kt
+++ PhotoGallery/app/src/main/java/com/ohhhzenix/csc475/photogallery/InternetScreen.kt
@@ -1,30 +1,40 @@
 package com.ohhhzenix.csc475.photogallery
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.ohhhzenix.csc475.photogallery.api.RetrofitClient
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,7 +42,8 @@ fun InternetScreen(
     navHomeScreen: () -> Unit
 ) {
     val context = LocalContext.current
-    var searchQuery by remember { mutableStateOf("") }
+    val coroutineScope = rememberCoroutineScope()
+    var images by remember { mutableStateOf<List<String>>(emptyList()) }
 
     Scaffold(
         topBar = {
@@ -67,36 +78,57 @@ fun InternetScreen(
                     fontSize = 32.sp
                 )
             )
-            OutlinedTextField(
-                value = searchQuery,
-                onValueChange = { searchQuery = it },
-                label = {
-                    Text("Search")
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 8.dp, end = 8.dp)
-            )
             Button(
                 onClick = {
-                    if (searchQuery.isEmpty()) {
-                        Toast.makeText(context, "Search is empty. Try again.", Toast.LENGTH_SHORT)
-                            .show()
-                    } else {
-
+                    coroutineScope.launch {
+                        val result = RetrofitClient.dogApi.getTenRandomDogImages();
+                        if (result.isSuccessful) {
+                            result.body()?.let {
+                                images = it.message
+                            }
+                        } else {
+                            Toast.makeText(
+                                context,
+                                "Failed to get images. Try later.",
+                                Toast.LENGTH_SHORT
+                            ).show();
+                        }
                     }
                 },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
             ) {
-                Text("Search")
+                Text("Get random dog images")
             }
-            Text(
-                "No results.",
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
+            if (images.isEmpty()) {
+                Text(
+                    "No results.",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            } else {
+                Text(
+                    "Image results:",
+                    modifier = Modifier.offset(x = 8.dp)
+                )
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(images) {
+                        AsyncImage(
+                            model = it,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(250.dp)
+                                .padding(8.dp)
+                                .background(Color.Black),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+            }
         }
     }
 }
