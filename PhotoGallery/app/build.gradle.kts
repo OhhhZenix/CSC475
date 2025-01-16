05d9a5264107ae9b1945f206bdabc09d9d09a7ac
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-16 14:55:25 -0700

    Update build.gradle.kts

--- PhotoGallery/app/build.gradle.kts
+++ PhotoGallery/app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
