368819f54d4e77eed331419536a5c483292c5df9
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-23 12:04:20 -0700

    Create AppScreen.kt

---
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/AppScreen.kt
@@ -0,0 +1,10 @@
+package com.ohhhzenix.csc475.fitnesstracker
+
+enum class AppScreen {
+    Home,
+    MealLogging,
+    ExerciseLogging,
+    CalculateGoals,
+    FoodCatalog,
+    ExerciseCatalog,
+}
