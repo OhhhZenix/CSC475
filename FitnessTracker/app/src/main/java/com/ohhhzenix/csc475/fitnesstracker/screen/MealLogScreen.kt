8ba2b0a8ae4f72ac3528da5ce5b59c22cd5463d1
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-23 16:02:30 -0700

    Update MealLogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/MealLogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/MealLogScreen.kt
@@ -1,7 +1,14 @@
 package com.ohhhzenix.csc475.fitnesstracker.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -9,11 +16,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MealLogScreen() {
+fun MealLogScreen(
+    navHomeScreen: () -> Unit
+) {
+    var mealsFromToday = emptyList<String>()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -29,6 +45,61 @@ fun MealLogScreen() {
     ) { innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding)
-        ) { }
+        ) {
+            Button(
+                onClick = navHomeScreen,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text("Back")
+            }
+            Spacer(Modifier.padding(8.dp))
+            Text(
+                "Today's Intake",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = 32.sp
+            )
+            Text(
+                "Total Calories: 0",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontStyle = FontStyle.Italic
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text("Add meal")
+            }
+            if (mealsFromToday.isEmpty()) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+
+                    ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            "No logs available from today.",
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            "Get started by adding one.",
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            } else {
+                Text(
+                    "Meals from Today:",
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                LazyColumn { }
+            }
+        }
     }
 }
