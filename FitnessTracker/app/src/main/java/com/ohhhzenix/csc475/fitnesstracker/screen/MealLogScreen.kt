b7bdf085601094e085c0cd6f0e16081685d0cc1e
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-23 14:09:13 -0700

    Update MealLogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/MealLogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/MealLogScreen.kt
@@ -1,4 +1,34 @@
 package com.ohhhzenix.csc475.fitnesstracker.screen
 
-class MealLogScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MealLogScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Meal Log")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) { }
+    }
 }
