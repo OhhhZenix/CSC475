c6d73792a290c71beff8a004081e16f5a90f706b
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-28 22:44:40 -0700

    Create FoodCatalogDao.kt

---
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/FoodCatalogDao.kt
@@ -0,0 +1,26 @@
+package com.ohhhzenix.csc475.fitnesstracker.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface FoodCatalogDao {
+
+    // add, get, update, delete
+
+    @Upsert
+    suspend fun addFood(food: FoodCatalog)
+
+    @Query("SELECT * FROM food_catalogs WHERE id = :id")
+    suspend fun getFood(id: Int): FoodCatalog?
+
+    @Query("SELECT * FROM food_catalogs")
+    suspend fun getAllFood(): List<FoodCatalog>
+
+    @Query("UPDATE food_catalogs SET name = :name, calories = :calories WHERE id = :id")
+    suspend fun updateFood(id: Int, name: String, calories: Double)
+
+    @Query("DELETE FROM food_catalogs WHERE id = :id")
+    suspend fun removeFood(id: Int)
+}
