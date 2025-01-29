aaab2c6dfbf056c354cbfaf06565b5d837e89c85
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 12:05:37 -0700

    Update FoodCatalogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/catalog/food/FoodCatalogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/catalog/food/FoodCatalogScreen.kt
@@ -17,19 +17,38 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.ohhhzenix.csc475.fitnesstracker.AppScreen
 import com.ohhhzenix.csc475.fitnesstracker.database.catalog.food.FoodCatalog
+import com.ohhhzenix.csc475.fitnesstracker.database.catalog.food.FoodCatalogDao
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FoodCatalogScreen(navController: NavController) {
-    val food = remember { mutableListOf<FoodCatalog>() }
+fun FoodCatalogScreen(
+    navController: NavController,
+    foodCatalogDao: FoodCatalogDao,
+    selectedFoodId: MutableIntState
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val food = remember { mutableStateListOf<FoodCatalog>() }
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            food.addAll(foodCatalogDao.getAllFood())
+        }
+    }
 
     Scaffold(topBar = {
         TopAppBar(
@@ -85,9 +104,34 @@ fun FoodCatalogScreen(navController: NavController) {
                 Text(
                     "Currently available selection:", modifier = Modifier.padding(start = 8.dp)
                 )
+                Spacer(Modifier.padding(4.dp))
                 LazyColumn {
                     items(food.sortedBy { it.name }) {
-
+                        Button(
+                            onClick = {
+                                selectedFoodId.intValue = it.id
+                                navController.navigate(AppScreen.EditFood.name)
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    it.name,
+                                    fontSize = 16.sp,
+                                    textAlign = TextAlign.Center,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "${it.calories} Calories",
+                                    textAlign = TextAlign.Center,
+                                    fontWeight = FontWeight.Light
+                                )
+                            }
+                        }
                     }
                 }
             }
