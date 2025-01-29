d8148edaa7bcfa3a005e0173f58c644c7c528f39
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 13:24:27 -0700

    Update ExerciseCatalog.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/catalog/exercise/ExerciseCatalog.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/catalog/exercise/ExerciseCatalog.kt
@@ -1,4 +1,19 @@
 package com.ohhhzenix.csc475.fitnesstracker.database.catalog.exercise
 
-class ExerciseCatalog {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "exercise_catalogs"
+)
+data class ExerciseCatalog(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "calories")
+    val calories: Double,
+)
