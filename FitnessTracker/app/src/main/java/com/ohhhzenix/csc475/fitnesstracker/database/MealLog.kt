2f9bc97e450595e79a6b910bac5544cca1292ece
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-28 22:44:48 -0700

    Update MealLog.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/MealLog.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/MealLog.kt
@@ -5,13 +5,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 
-@Entity(tableName = "meal_logs")
+@Entity(
+    tableName = "meal_logs",
+)
 data class MealLog(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 
     @ColumnInfo(name = "date_time")
-    val date: LocalDateTime = LocalDateTime.now(),
+    val dateTime: LocalDateTime,
 
     @ColumnInfo(name = "food_id")
     val foodId: Int,
