6021700ed9012c836cde49e9bfd7bcadb26abbf8
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 09:37:44 -0700

    Update MealLog.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/MealLog.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/MealLog.kt
@@ -3,7 +3,6 @@ package com.ohhhzenix.csc475.fitnesstracker.database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.time.LocalDateTime
 
 @Entity(
     tableName = "meal_logs",
