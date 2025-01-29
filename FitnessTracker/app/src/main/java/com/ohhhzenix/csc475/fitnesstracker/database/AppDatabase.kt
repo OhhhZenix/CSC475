12db8017263141ed1dc4c44d5071d3de925fc9d3
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 11:04:42 -0700

    Update AppDatabase.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/AppDatabase.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/AppDatabase.kt
@@ -3,19 +3,27 @@ package com.ohhhzenix.csc475.fitnesstracker.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.ohhhzenix.csc475.fitnesstracker.database.catalog.food.FoodCatalog
+import com.ohhhzenix.csc475.fitnesstracker.database.catalog.food.FoodCatalogDao
 import com.ohhhzenix.csc475.fitnesstracker.database.converters.LocalDateTimeConverter
 import com.ohhhzenix.csc475.fitnesstracker.database.log.meal.MealLog
 import com.ohhhzenix.csc475.fitnesstracker.database.log.meal.MealLogDao
 
 @Database(
-    entities = [MealLog::class],
+    entities = [
+        MealLog::class,
+        FoodCatalog::class
+    ],
     version = 1
 )
 @TypeConverters(LocalDateTimeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
+
     companion object {
         const val NAME = "fitness_tracker"
     }
 
     abstract fun getMealLogDao(): MealLogDao
+
+    abstract fun getFoodCatalogDao(): FoodCatalogDao
 }
