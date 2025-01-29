2d27150d2b557eeeb5014603d432c4170868826e
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 09:56:56 -0700

    Update MealLogDao.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/MealLogDao.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/MealLogDao.kt
@@ -7,11 +7,17 @@ import androidx.room.Upsert
 @Dao
 interface MealLogDao {
 
-    @Query("SELECT * FROM meal_logs WHERE date_time = now()")
-    suspend fun getTodayList(): List<MealLog>
-
     @Upsert
     suspend fun addLog(log: MealLog)
+
+    @Query("SELECT * FROM meal_logs WHERE cast(date_time as Date) = cast(getdate() as Date)")
+    suspend fun getTodayLogs(): List<MealLog>
+
+    @Query("SELECT * FROM meal_logs")
+    suspend fun getAllLogs(): List<MealLog>
+
+    @Query("UPDATE meal_logs SET food_id = :foodId where id = :id")
+    suspend fun updateLog(id: Int, foodId: Int)
 
     @Query("DELETE FROM meal_logs where id = :id")
     suspend fun removeLog(id: Int)
