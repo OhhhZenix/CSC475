a9c5f4dc1aab935f0c1b5019e649371c78698c19
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 10:44:25 -0700

    Update FoodCatalogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/FoodCatalogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/FoodCatalogScreen.kt
@@ -1,9 +1,14 @@
 package com.ohhhzenix.csc475.fitnesstracker.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -21,6 +28,8 @@ import com.ohhhzenix.csc475.fitnesstracker.AppScreen
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FoodCatalogScreen(navController: NavController) {
+    val food = remember { mutableListOf<String>() }
+
     Scaffold(topBar = {
         TopAppBar(
             title = {
@@ -48,10 +57,38 @@ fun FoodCatalogScreen(navController: NavController) {
                 textAlign = TextAlign.Center,
             )
             Button(
-                onClick = {},
-                modifier = Modifier.padding(8.dp).fillMaxWidth()
+                onClick = { navController.navigate(AppScreen.AddFood.name) },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
             ) {
                 Text("Add Food")
+            }
+            if (food.isEmpty()) {
+                Box(
+                    modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            "No food exists currently.", textAlign = TextAlign.Center
+                        )
+                        Text(
+                            "Get started by adding one.", textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            } else {
+                Text(
+                    "Currently available selection:", modifier = Modifier.padding(start = 8.dp)
+                )
+                LazyColumn {
+                    items(food) {
+
+                    }
+                }
             }
         }
     }
