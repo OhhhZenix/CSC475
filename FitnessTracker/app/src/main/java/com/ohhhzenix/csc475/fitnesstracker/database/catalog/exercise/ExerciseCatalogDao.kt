824028f99628c916bb1c886553245cdee10f30ac
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 13:24:28 -0700

    Update ExerciseCatalogDao.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/catalog/exercise/ExerciseCatalogDao.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/catalog/exercise/ExerciseCatalogDao.kt
@@ -1,4 +1,24 @@
 package com.ohhhzenix.csc475.fitnesstracker.database.catalog.exercise
 
-class ExerciseCatalogDao {
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface ExerciseCatalogDao {
+
+    @Upsert
+    suspend fun addExercise(exercise: ExerciseCatalog)
+
+    @Query("SELECT * FROM exercise_catalogs WHERE id = :id")
+    suspend fun getExercise(id: Int): ExerciseCatalog?
+
+    @Query("SELECT * FROM exercise_catalogs")
+    suspend fun getAllExercise(): List<ExerciseCatalog>
+
+    @Query("UPDATE exercise_catalogs SET name = :name, calories = :calories WHERE id = :id")
+    suspend fun updateExercise(id: Int, name: String, calories: Double)
+
+    @Query("DELETE FROM exercise_catalogs WHERE id = :id")
+    suspend fun removeExercise(id: Int)
 }
