95694e7081196716fe54db871f56933a99550f67
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 09:45:44 -0700

    Update AppDatabase.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/AppDatabase.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/AppDatabase.kt
@@ -2,11 +2,13 @@ package com.ohhhzenix.csc475.fitnesstracker.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [MealLog::class],
     version = 1
 )
+@TypeConverters(LocalDateTimeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
         const val NAME = "fitness_tracker"
