399c07e222f61c05f431f27f44e6ae0b0e7a6406
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 11:12:42 -0700

    Update AddFoodCatalogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/catalog/food/AddFoodCatalogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/catalog/food/AddFoodCatalogScreen.kt
@@ -1,6 +1,34 @@
 package com.ohhhzenix.csc475.fitnesstracker.screen.catalog.food
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddFoodCatalogScreen() {}
+fun AddFoodCatalogScreen() {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text("Food Catalog")
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+        )
+    }) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+    }
+}
