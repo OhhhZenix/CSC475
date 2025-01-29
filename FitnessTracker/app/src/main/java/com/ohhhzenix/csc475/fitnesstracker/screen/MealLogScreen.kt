1a26880806966476afaf3558e4bc38e76ec781a0
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 10:19:31 -0700

    Update MealLogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/MealLogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/MealLogScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -16,38 +17,54 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.ohhhzenix.csc475.fitnesstracker.AppScreen
+import com.ohhhzenix.csc475.fitnesstracker.database.MealLog
+import com.ohhhzenix.csc475.fitnesstracker.database.MealLogDao
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MealLogScreen(
-    navHomeScreen: () -> Unit
+    navController: NavController,
+    mealLogDao: MealLogDao,
 ) {
-    var mealsFromToday = emptyList<String>()
+    val mealsFromToday = remember { mutableStateListOf<MealLog>() }
+    val coroutineScope = rememberCoroutineScope()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text("Meal Log")
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-            )
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            mealsFromToday.clear()
+            mealsFromToday.addAll(mealLogDao.getTodayLogs())
         }
-    ) { innerPadding ->
+    }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text("Meal Log")
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+        )
+    }) { innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
             Button(
-                onClick = navHomeScreen,
+                onClick = { navController.navigate(AppScreen.Home.name) },
                 modifier = Modifier.padding(8.dp)
             ) {
                 Text("Back")
@@ -66,8 +83,7 @@ fun MealLogScreen(
                 fontStyle = FontStyle.Italic
             )
             Button(
-                onClick = {},
-                modifier = Modifier
+                onClick = {}, modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
             ) {
@@ -84,21 +100,22 @@ fun MealLogScreen(
                         verticalArrangement = Arrangement.Center
                     ) {
                         Text(
-                            "No logs available from today.",
-                            textAlign = TextAlign.Center
+                            "No logs available from today.", textAlign = TextAlign.Center
                         )
                         Text(
-                            "Get started by adding one.",
-                            textAlign = TextAlign.Center
+                            "Get started by adding one.", textAlign = TextAlign.Center
                         )
                     }
                 }
             } else {
                 Text(
-                    "Meals from Today:",
-                    modifier = Modifier.padding(start = 8.dp)
+                    "Meals from Today:", modifier = Modifier.padding(start = 8.dp)
                 )
-                LazyColumn { }
+                LazyColumn {
+                    items(mealsFromToday) {
+
+                    }
+                }
             }
         }
     }
