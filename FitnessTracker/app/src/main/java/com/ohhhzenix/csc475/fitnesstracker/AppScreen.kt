e3b3b296c09a7075c7d6d229e1e71b7474bafe25
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-29 10:35:56 -0700

    Update AppScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/AppScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/AppScreen.kt
@@ -2,9 +2,21 @@ package com.ohhhzenix.csc475.fitnesstracker
 
 enum class AppScreen {
     Home,
-    MealLog,
-    ExerciseLog,
     CalculateGoal,
+
+    MealLog,
+    AddMealLog,
+    EditMealLog,
+
+    ExerciseLog,
+    AddExerciseLog,
+    EditExerciseLog,
+
     FoodCatalog,
+    AddFood,
+    EditFood,
+
     ExerciseCatalog,
+    AddExercise,
+    EditExercise,
 }
