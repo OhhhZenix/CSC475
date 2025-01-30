af45b4159aaaa46cd60f53819711846f90cc55be
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-30 15:52:17 -0700

    Update ExerciseLog.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/log/exercise/ExerciseLog.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/database/log/exercise/ExerciseLog.kt
@@ -1,4 +1,23 @@
 package com.ohhhzenix.csc475.fitnesstracker.database.log.exercise
 
-class ExerciseLog {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "exercise_logs",
+)
+data class ExerciseLog(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @ColumnInfo(name = "date_time")
+    val dateTime: LocalDateTime,
+
+    @ColumnInfo(name = "exercise_id")
+    val exerciseId: Int,
+
+    @ColumnInfo(name = "quantity")
+    val quantity: Double,
+)
