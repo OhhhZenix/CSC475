97b9c79bc76c348edeb4bc7fc9fffa73869cdde0
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-30 15:42:13 -0700

    Update EditFoodCatalogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/catalog/food/EditFoodCatalogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/catalog/food/EditFoodCatalogScreen.kt
@@ -67,7 +67,7 @@ fun EditFoodCatalogScreen(
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
-            Spacer(Modifier.padding(8.dp))
+            Spacer(Modifier.padding(4.dp))
             Text(
                 "(Selected Food ID: ${selectedFoodCatalog.intValue})",
                 modifier = Modifier.fillMaxWidth(),
@@ -106,10 +106,14 @@ fun EditFoodCatalogScreen(
                     if (name.value.isEmpty()) {
                         Toast.makeText(context, "Name is empty. Try again.", Toast.LENGTH_SHORT)
                             .show()
+                    } else if (calories.value.isEmpty()) {
+
+                        Toast.makeText(context, "Calories is empty. Try again.", Toast.LENGTH_SHORT)
+                            .show()
                     } else if (calories.value.toDouble() <= 0.0) {
                         Toast.makeText(
                             context,
-                            "Calories must be greater than zero. Try again.",
+                            "Calories is less than or equal to zero. Try again.",
                             Toast.LENGTH_SHORT
                         )
                             .show()
