a1ed954953bd6c627b6d3b0354bf459352a41a85
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-30 15:42:06 -0700

    Update CalculateGoalScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/CalculateGoalScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/CalculateGoalScreen.kt
@@ -2,5 +2,14 @@ package com.ohhhzenix.csc475.fitnesstracker.screen
 
 import androidx.compose.runtime.Composable
 
+fun calculateMaleBMR(weight: Double, height: Double, age: Double): Double {
+    return 10 * weight + 6.25 * height - 5 * age + 5
+}
+
+fun calculateFemaleBMR(weight: Double, height: Double, age: Double): Double {
+    return 10 * weight + 6.25 * height - 5 * age - 161
+}
+
 @Composable
-fun CalculateGoalScreen() {}
+fun CalculateGoalScreen() {
+}
