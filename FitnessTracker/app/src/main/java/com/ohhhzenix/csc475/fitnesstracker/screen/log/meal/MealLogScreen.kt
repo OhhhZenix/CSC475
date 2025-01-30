c581d3daec10719dbad1897342a2d0a7fc23819e
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-30 13:43:52 -0700

    Update MealLogScreen.kt

--- FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/log/meal/MealLogScreen.kt
+++ FitnessTracker/app/src/main/java/com/ohhhzenix/csc475/fitnesstracker/screen/log/meal/MealLogScreen.kt
@@ -3,12 +3,13 @@ package com.ohhhzenix.csc475.fitnesstracker.screen.log.meal
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -24,27 +25,35 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.ohhhzenix.csc475.fitnesstracker.AppScreen
+import com.ohhhzenix.csc475.fitnesstracker.database.catalog.food.FoodCatalog
+import com.ohhhzenix.csc475.fitnesstracker.database.catalog.food.FoodCatalogDao
 import com.ohhhzenix.csc475.fitnesstracker.database.log.meal.MealLog
 import com.ohhhzenix.csc475.fitnesstracker.database.log.meal.MealLogDao
 import kotlinx.coroutines.launch
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MealLogScreen(
     navController: NavController,
     mealLogDao: MealLogDao,
+    foodCatalogDao: FoodCatalogDao
 ) {
     val coroutineScope = rememberCoroutineScope()
-    val mealsFromToday = remember { mutableStateListOf<MealLog>() }
+    val meals = remember { mutableStateListOf<Pair<MealLog, FoodCatalog?>>() }
 
     LaunchedEffect(Unit) {
         coroutineScope.launch {
-            mealsFromToday.addAll(mealLogDao.getTodayLogs())
+            mealLogDao.getTodayLogs().forEach { log ->
+                val food = foodCatalogDao.getFood(log.foodId)
+                meals.add(Pair(log, food))
+            }
         }
     }
 
@@ -88,7 +97,7 @@ fun MealLogScreen(
             ) {
                 Text("Add meal")
             }
-            if (mealsFromToday.isEmpty()) {
+            if (meals.isEmpty()) {
                 Box(
                     modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
                 ) {
@@ -109,8 +118,46 @@ fun MealLogScreen(
                     "Meals from Today:", modifier = Modifier.padding(start = 8.dp)
                 )
                 LazyColumn {
-                    items(mealsFromToday) {
-
+                    items(meals.sortedBy { it.first.dateTime }.reversed()) {
+                        it.second?.let { food ->
+                            Button(
+                                onClick = {},
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+                                shape = RoundedCornerShape(8.dp)
+                            ) {
+                                Row(
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Column {
+                                        Text(
+                                            text = food.name,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 16.sp
+                                        )
+                                        Text(
+                                            text = "Quantity: ${it.first.quantity}",
+                                            fontWeight = FontWeight.Light
+                                        )
+                                        Text(
+                                            text = "Total Calories: ${food.calories * it.first.quantity}",
+                                            fontWeight = FontWeight.Light
+                                        )
+                                    }
+                                    Text(
+                                        text = it.first.dateTime.format(
+                                            DateTimeFormatter.ofPattern(
+                                                "MMM dd yyyy hh:mm a"
+                                            )
+                                        ),
+                                        textAlign = TextAlign.End,
+                                        modifier = Modifier.fillMaxWidth(),
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             }
