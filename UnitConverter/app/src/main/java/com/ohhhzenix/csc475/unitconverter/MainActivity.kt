0b572c559ae79552f034b88c5e4887225f14616c
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-01 21:50:31 -0700

    Update MainActivity.kt

--- UnitConverter/app/src/main/java/com/ohhhzenix/csc475/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/ohhhzenix/csc475/unitconverter/MainActivity.kt
@@ -194,6 +194,7 @@ fun metricTonToGram(metricTon: Double): Double = metricTon * 1_000_000
 fun metricTonToMilligram(metricTon: Double): Double = metricTon * 1_000_000_000
 fun metricTonToMicrogram(metricTon: Double): Double = metricTon * 1_000_000_000_000
 fun metricTonToImperialTon(metricTon: Double): Double = metricTon * 0.984207
+fun metricTonToUSTon(metricTon: Double): Double = metricTon * 1.10231
 fun metricTonToStone(metricTon: Double): Double = metricTon * 157.473
 fun metricTonToPound(metricTon: Double): Double = metricTon * 2204.62
 fun metricTonToOunce(metricTon: Double): Double = metricTon * 35_273.96
@@ -204,6 +205,7 @@ fun kilogramToGram(kg: Double): Double = kg * 1000
 fun kilogramToMilligram(kg: Double): Double = kg * 1_000_000
 fun kilogramToMicrogram(kg: Double): Double = kg * 1_000_000_000
 fun kilogramToImperialTon(kg: Double): Double = kg / 1016.05
+fun kilogramToUSTon(kg: Double): Double = kg / 907.18474
 fun kilogramToStone(kg: Double): Double = kg / 6.35
 fun kilogramToPound(kg: Double): Double = kg * 2.20462
 fun kilogramToOunce(kg: Double): Double = kg * 35.274
@@ -214,6 +216,7 @@ fun gramToKilogram(g: Double): Double = g / 1000
 fun gramToMilligram(g: Double): Double = g * 1000
 fun gramToMicrogram(g: Double): Double = g * 1_000_000
 fun gramToImperialTon(g: Double): Double = g / 1_016_050
+fun gramToUSTon(g: Double): Double = g / 907_184.74
 fun gramToStone(g: Double): Double = g / 6350.293
 fun gramToPound(g: Double): Double = g / 453.592
 fun gramToOunce(g: Double): Double = g / 28.3495
@@ -224,6 +227,7 @@ fun milligramToKilogram(mg: Double): Double = mg / 1_000_000
 fun milligramToGram(mg: Double): Double = mg / 1000
 fun milligramToMicrogram(mg: Double): Double = mg * 1000
 fun milligramToImperialTon(mg: Double): Double = mg / 1_016_050_000
+fun milligramToUSTon(mg: Double): Double = mg / 907_184_740
 fun milligramToStone(mg: Double): Double = mg / 6_350_293
 fun milligramToPound(mg: Double): Double = mg / 453_592_370
 fun milligramToOunce(mg: Double): Double = mg / 28_349_523.125
@@ -234,6 +238,7 @@ fun microgramToKilogram(ug: Double): Double = ug / 1_000_000_000
 fun microgramToGram(ug: Double): Double = ug / 1_000_000
 fun microgramToMilligram(ug: Double): Double = ug / 1000
 fun microgramToImperialTon(ug: Double): Double = ug / 1_016_050_000_000
+fun microgramToUSTon(ug: Double): Double = ug / 907_184_740_000
 fun microgramToStone(ug: Double): Double = ug / 6_350_293_000
 fun microgramToPound(ug: Double): Double = ug / 453_592_370_000
 fun microgramToOunce(ug: Double): Double = ug / 28_349_523_125
@@ -244,9 +249,21 @@ fun imperialTonToKilogram(impTon: Double): Double = impTon * 1016.05
 fun imperialTonToGram(impTon: Double): Double = impTon * 1_016_050
 fun imperialTonToMilligram(impTon: Double): Double = impTon * 1_016_050_000
 fun imperialTonToMicrogram(impTon: Double): Double = impTon * 1_016_050_000_000
+fun imperialTonToUSTon(impTon: Double): Double = impTon * 1.119
 fun imperialTonToStone(impTon: Double): Double = impTon * 20.0000
 fun imperialTonToPound(impTon: Double): Double = impTon * 2_240
 fun imperialTonToOunce(impTon: Double): Double = impTon * 35_840
+
+// US Ton (short ton) to other units
+fun usTonToMetricTon(usTon: Double): Double = usTon / 1.10231
+fun usTonToKilogram(usTon: Double): Double = usTon * 907.18474
+fun usTonToGram(usTon: Double): Double = usTon * 907_184.74
+fun usTonToMilligram(usTon: Double): Double = usTon * 907_184_740
+fun usTonToMicrogram(usTon: Double): Double = usTon * 907_184_740_000
+fun usTonToImperialTon(usTon: Double): Double = usTon / 1.119
+fun usTonToStone(usTon: Double): Double = usTon * 142.857
+fun usTonToPound(usTon: Double): Double = usTon * 2000
+fun usTonToOunce(usTon: Double): Double = usTon * 32_000
 
 // Stone to other units
 fun stoneToMetricTon(stone: Double): Double = stone / 157.473
@@ -255,6 +272,7 @@ fun stoneToGram(stone: Double): Double = stone * 6350.293
 fun stoneToMilligram(stone: Double): Double = stone * 6_350_293
 fun stoneToMicrogram(stone: Double): Double = stone * 6_350_293_000
 fun stoneToImperialTon(stone: Double): Double = stone / 20
+fun stoneToUSTon(stone: Double): Double = stone / 142.857
 fun stoneToPound(stone: Double): Double = stone * 14
 fun stoneToOunce(stone: Double): Double = stone * 224
 
@@ -265,6 +283,7 @@ fun poundToGram(pound: Double): Double = pound * 453.592
 fun poundToMilligram(pound: Double): Double = pound * 453_592
 fun poundToMicrogram(pound: Double): Double = pound * 453_592_370
 fun poundToImperialTon(pound: Double): Double = pound / 2240
+fun poundToUSTon(pound: Double): Double = pound / 2000
 fun poundToStone(pound: Double): Double = pound / 14
 fun poundToOunce(pound: Double): Double = pound * 16
 
@@ -273,8 +292,9 @@ fun ounceToMetricTon(ounce: Double): Double = ounce / 35_273.96
 fun ounceToKilogram(ounce: Double): Double = ounce / 35.274
 fun ounceToGram(ounce: Double): Double = ounce * 28.3495
 fun ounceToMilligram(ounce: Double): Double = ounce * 28_349.5
-fun ounceToMicrogram(ounce: Double): Double = ounce * 28_349_523.125
+fun ounceToMicrogram(ounce: Double): Double = ounce * 28_349_523_125
 fun ounceToImperialTon(ounce: Double): Double = ounce / 35_840
+fun ounceToUSTon(ounce: Double): Double = ounce / 32_000
 fun ounceToStone(ounce: Double): Double = ounce / 224
 fun ounceToPound(ounce: Double): Double = ounce / 16
 
@@ -345,10 +365,242 @@ fun TemperatureUnits() {
 
 @Composable
 fun LengthUnits() {
+    var metricTon by remember { mutableStateOf("") }
+    var kilogram by remember { mutableStateOf("") }
+    var gram by remember { mutableStateOf("") }
+    var milligram by remember { mutableStateOf("") }
+    var microgram by remember { mutableStateOf("") }
+    var imperialTon by remember { mutableStateOf("") }
+    var usTon by remember { mutableStateOf("") }
+    var stone by remember { mutableStateOf("") }
+    var pound by remember { mutableStateOf("") }
+    var ounce by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = metricTon,
+        onValueChange = {
+            if (isDouble(it)) {
+                metricTon = it
+                kilogram = metricTonToKilogram(metricTon.toDouble()).toString()
+                gram = metricTonToGram(metricTon.toDouble()).toString()
+                milligram = metricTonToMilligram(metricTon.toDouble()).toString()
+                microgram = metricTonToMicrogram(metricTon.toDouble()).toString()
+                imperialTon = metricTonToImperialTon(metricTon.toDouble()).toString()
+                usTon = metricTonToUSTon(metricTon.toDouble()).toString()
+                stone = metricTonToStone(metricTon.toDouble()).toString()
+                pound = metricTonToPound(metricTon.toDouble()).toString()
+                ounce = metricTonToOunce(metricTon.toDouble()).toString()
+            }
+        },
+        label = { Text("Metric ton") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = kilogram,
+        onValueChange = {
+            if (isDouble(it)) {
+                kilogram = it
+                metricTon = kilogramToMetricTon(kilogram.toDouble()).toString()
+                gram = kilogramToGram(kilogram.toDouble()).toString()
+                milligram = kilogramToMilligram(kilogram.toDouble()).toString()
+                microgram = kilogramToMicrogram(kilogram.toDouble()).toString()
+                imperialTon = kilogramToImperialTon(kilogram.toDouble()).toString()
+                usTon = kilogramToUSTon(kilogram.toDouble()).toString()
+                pound = kilogramToPound(kilogram.toDouble()).toString()
+                ounce = kilogramToOunce(kilogram.toDouble()).toString()
+            }
+        },
+        label = { Text("Kilogram") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = gram,
+        onValueChange = {
+            if (isDouble(it)) {
+                gram = it
+                metricTon = gramToMetricTon(gram.toDouble()).toString()
+                kilogram = gramToKilogram(gram.toDouble()).toString()
+                milligram = gramToMilligram(gram.toDouble()).toString()
+                microgram = gramToMicrogram(gram.toDouble()).toString()
+                imperialTon = gramToImperialTon(gram.toDouble()).toString()
+                usTon = gramToUSTon(gram.toDouble()).toString()
+                stone = gramToStone(gram.toDouble()).toString()
+                pound = gramToPound(gram.toDouble()).toString()
+                ounce = gramToOunce(gram.toDouble()).toString()
+            }
+        },
+        label = { Text("Gram") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = milligram,
+        onValueChange = {
+            if (isDouble(it)) {
+                milligram = it
+                metricTon = milligramToMetricTon(milligram.toDouble()).toString()
+                kilogram = milligramToKilogram(milligram.toDouble()).toString()
+                gram = milligramToGram(milligram.toDouble()).toString()
+                microgram = milligramToMicrogram(milligram.toDouble()).toString()
+                imperialTon = milligramToImperialTon(milligram.toDouble()).toString()
+                usTon = milligramToUSTon(milligram.toDouble()).toString()
+                stone = milligramToStone(milligram.toDouble()).toString()
+                pound = milligramToPound(milligram.toDouble()).toString()
+                ounce = milligramToOunce(milligram.toDouble()).toString()
+            }
+        },
+        label = { Text("Milligram") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = microgram,
+        onValueChange = {
+            if (isDouble(it)) {
+                microgram = it
+                metricTon = microgramToMetricTon(microgram.toDouble()).toString()
+                kilogram = microgramToKilogram(microgram.toDouble()).toString()
+                gram = microgramToGram(microgram.toDouble()).toString()
+                milligram = microgramToMilligram(microgram.toDouble()).toString()
+                imperialTon = microgramToImperialTon(microgram.toDouble()).toString()
+                usTon = microgramToUSTon(microgram.toDouble()).toString()
+                stone = microgramToStone(microgram.toDouble()).toString()
+                pound = microgramToPound(microgram.toDouble()).toString()
+                ounce = microgramToOunce(microgram.toDouble()).toString()
+            }
+        },
+        label = { Text("Microgram") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = imperialTon,
+        onValueChange = {
+            if (isDouble(it)) {
+                imperialTon = it
+                metricTon = imperialTonToMetricTon(imperialTon.toDouble()).toString()
+                kilogram = imperialTonToKilogram(imperialTon.toDouble()).toString()
+                gram = imperialTonToGram(imperialTon.toDouble()).toString()
+                milligram = imperialTonToMilligram(imperialTon.toDouble()).toString()
+                microgram = imperialTonToMicrogram(imperialTon.toDouble()).toString()
+                usTon = imperialTonToUSTon(imperialTon.toDouble()).toString()
+                stone = imperialTonToStone(imperialTon.toDouble()).toString()
+                pound = imperialTonToPound(imperialTon.toDouble()).toString()
+                ounce = imperialTonToOunce(imperialTon.toDouble()).toString()
+            }
+        },
+        label = { Text("Imperial ton") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = usTon,
+        onValueChange = {
+            if (isDouble(it)) {
+                usTon = it
+                metricTon = usTonToMetricTon(usTon.toDouble()).toString()
+                kilogram = usTonToKilogram(usTon.toDouble()).toString()
+                gram = usTonToGram(usTon.toDouble()).toString()
+                milligram = usTonToMilligram(usTon.toDouble()).toString()
+                microgram = usTonToMicrogram(usTon.toDouble()).toString()
+                imperialTon = usTonToImperialTon(usTon.toDouble()).toString()
+                stone = usTonToStone(usTon.toDouble()).toString()
+                pound = usTonToPound(usTon.toDouble()).toString()
+                ounce = usTonToOunce(usTon.toDouble()).toString()
+            }
+        },
+        label = { Text("US ton") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = stone,
+        onValueChange = {
+            if (isDouble(it)) {
+                stone = it
+                metricTon = stoneToMetricTon(stone.toDouble()).toString()
+                kilogram = stoneToKilogram(stone.toDouble()).toString()
+                gram = stoneToGram(stone.toDouble()).toString()
+                milligram = stoneToMilligram(stone.toDouble()).toString()
+                microgram = stoneToMicrogram(stone.toDouble()).toString()
+                imperialTon = stoneToImperialTon(stone.toDouble()).toString()
+                usTon = stoneToUSTon(stone.toDouble()).toString()
+                pound = stoneToPound(stone.toDouble()).toString()
+                ounce = stoneToOunce(stone.toDouble()).toString()
+            }
+        },
+        label = { Text("Stone") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = pound,
+        onValueChange = {
+            if (isDouble(it)) {
+                pound = it
+                metricTon = poundToMetricTon(pound.toDouble()).toString()
+                kilogram = poundToKilogram(pound.toDouble()).toString()
+                gram = poundToGram(pound.toDouble()).toString()
+                milligram = poundToMilligram(pound.toDouble()).toString()
+                microgram = poundToMicrogram(pound.toDouble()).toString()
+                imperialTon = poundToImperialTon(pound.toDouble()).toString()
+                usTon = poundToUSTon(pound.toDouble()).toString()
+                stone = poundToStone(pound.toDouble()).toString()
+                ounce = poundToOunce(pound.toDouble()).toString()
+            }
+        },
+        label = { Text("Pound") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    OutlinedTextField(
+        value = ounce,
+        onValueChange = {
+            if (isDouble(it)) {
+                ounce = it
+                metricTon = ounceToMetricTon(ounce.toDouble()).toString()
+                kilogram = ounceToKilogram(ounce.toDouble()).toString()
+                gram = ounceToGram(ounce.toDouble()).toString()
+                milligram = ounceToMilligram(ounce.toDouble()).toString()
+                microgram = ounceToMicrogram(ounce.toDouble()).toString()
+                imperialTon = ounceToImperialTon(ounce.toDouble()).toString()
+                usTon = ounceToUSTon(ounce.toDouble()).toString()
+                stone = ounceToStone(ounce.toDouble()).toString()
+                pound = ounceToPound(ounce.toDouble()).toString()
+            }
+        },
+        label = { Text("Ounce") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 @Composable
 fun MassUnits() {
+
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
