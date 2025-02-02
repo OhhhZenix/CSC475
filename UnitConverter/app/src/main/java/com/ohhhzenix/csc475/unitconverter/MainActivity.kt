baf53762f8159decf4c1b4c3268aef1570839051
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-01 17:39:08 -0700

    Update MainActivity.kt

--- UnitConverter/app/src/main/java/com/ohhhzenix/csc475/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/ohhhzenix/csc475/unitconverter/MainActivity.kt
@@ -6,15 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.ohhhzenix.csc475.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,6 +70,9 @@ fun kelvinToFahrenheit(kelvin: Double): Double {
 @Composable
 fun App() {
     val options = listOf("Temperature", "Length", "Mass");
+    var celsius by remember { mutableStateOf("") }
+    var fahrenheit by remember { mutableStateOf("") }
+    var kelvin by remember { mutableStateOf("") }
 
     Scaffold(
         topBar = {
@@ -80,6 +92,63 @@ fun App() {
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
+            OutlinedTextField(
+                value = celsius,
+                onValueChange = {
+                    if (isDouble(it)) {
+                        celsius = it
+                        fahrenheit = celsiusToFahrenheit(celsius.toDouble()).toString()
+                        kelvin = celsiusToKelvin(celsius.toDouble()).toString()
+                    }
+                },
+                label = {
+                    Text("Celsius")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+            OutlinedTextField(
+                value = fahrenheit,
+                onValueChange = {
+                    if (isDouble(it)) {
+                        fahrenheit = it
+                        celsius = fahrenheitToCelsius(fahrenheit.toDouble()).toString()
+                        kelvin = fahrenheitToKelvin(fahrenheit.toDouble()).toString()
+                    }
+                },
+                label = {
+                    Text("Fahrenheit")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+            OutlinedTextField(
+                value = kelvin,
+                onValueChange = {
+                    if (isDouble(it)) {
+                        kelvin = it
+                        celsius = kelvinToCelsius(kelvin.toDouble()).toString()
+                        fahrenheit = kelvinToFahrenheit(kelvin.toDouble()).toString()
+                    }
+                },
+                label = {
+                    Text("Kelvin")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
         }
     }
 }
