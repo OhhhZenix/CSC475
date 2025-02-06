2a5a27be4a78c7fc203546766979ab15361f938c
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-06 15:44:47 -0700

    Update BMRTests.kt

--- FitnessTracker/app/src/test/java/com/ohhhzenix/csc475/fitnesstracker/BMRTests.kt
+++ FitnessTracker/app/src/test/java/com/ohhhzenix/csc475/fitnesstracker/BMRTests.kt
@@ -1,6 +1,17 @@
 package com.ohhhzenix.csc475.fitnesstracker
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class BMRTests {
 
-    
+    @Test
+    fun testMaleBMR() {
+        assertEquals(1857.45, calculateMaleBMR(90.62, 175.4, 30.0), 0.01)
+    }
+
+    @Test
+    fun testFemaleBMR() {
+        assertEquals(1629.00, calculateMaleBMR(77.4, 160.0, 30.0), 0.01)
+    }
 }
