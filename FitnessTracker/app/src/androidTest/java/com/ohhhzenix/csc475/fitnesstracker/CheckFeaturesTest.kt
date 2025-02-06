96b87ab076616123662ee9f502339dbf47f93e5e
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-06 16:13:05 -0700

    Update CheckFeaturesTest.kt

--- FitnessTracker/app/src/androidTest/java/com/ohhhzenix/csc475/fitnesstracker/CheckFeaturesTest.kt
+++ FitnessTracker/app/src/androidTest/java/com/ohhhzenix/csc475/fitnesstracker/CheckFeaturesTest.kt
@@ -1,1 +1,31 @@
+package com.ohhhzenix.csc475.fitnesstracker
 
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.rememberNavController
+import com.ohhhzenix.csc475.fitnesstracker.screen.HomeScreen
+import org.junit.Rule
+import org.junit.Test
+
+class CheckFeaturesTest {
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    private val calculateButton = hasText("Calculate Goals") and hasClickAction()
+    private val mealLogButton = hasText("Meal Log") and hasClickAction()
+    private val exerciseLogButton = hasText("Exercise Log") and hasClickAction()
+    private val foodCatalogButton = hasText("Food Catalog") and hasClickAction()
+    private val exerciseCatalogButton = hasText("Exercise Catalog") and hasClickAction()
+
+    @Test
+    fun testFeatures() {
+        rule.setContent { HomeScreen(navController = rememberNavController()) }
+        rule.onNode(calculateButton).assertExists()
+        rule.onNode(mealLogButton).assertExists()
+        rule.onNode(exerciseLogButton).assertExists()
+        rule.onNode(foodCatalogButton).assertExists()
+        rule.onNode(exerciseCatalogButton).assertExists()
+    }
+}
