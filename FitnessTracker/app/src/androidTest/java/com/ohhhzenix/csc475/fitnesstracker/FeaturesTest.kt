013511c1160060867e83759c3ea345b039a3a0e2
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-06 16:20:45 -0700

    Update FeaturesTest.kt

--- FitnessTracker/app/src/androidTest/java/com/ohhhzenix/csc475/fitnesstracker/FeaturesTest.kt
+++ FitnessTracker/app/src/androidTest/java/com/ohhhzenix/csc475/fitnesstracker/FeaturesTest.kt
@@ -3,7 +3,6 @@ package com.ohhhzenix.csc475.fitnesstracker
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.rememberNavController
 import com.ohhhzenix.csc475.fitnesstracker.screen.HomeScreen
 import org.junit.Rule
@@ -19,7 +18,6 @@ class FeaturesTest {
     private val exerciseLogButton = hasText("Exercise Log") and hasClickAction()
     private val foodCatalogButton = hasText("Food Catalog") and hasClickAction()
     private val exerciseCatalogButton = hasText("Exercise Catalog") and hasClickAction()
-    private val backButton = hasText("Back") and hasClickAction()
 
     @Test
     fun testFeaturesExists() {
@@ -29,12 +27,5 @@ class FeaturesTest {
         rule.onNode(exerciseLogButton).assertExists()
         rule.onNode(foodCatalogButton).assertExists()
         rule.onNode(exerciseCatalogButton).assertExists()
-    }
-
-    @Test
-    fun testCalculateFeature() {
-        rule.setContent { HomeScreen(navController = rememberNavController()) }
-        rule.onNode(calculateButton).performClick().assertExists()
-        rule.onNode(backButton).assertExists()
     }
 }
